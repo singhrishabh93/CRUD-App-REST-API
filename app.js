66b7e57c9cf3d725635c927b956355e971e8439a
singhrishabh93 <singhrishabh1670@gmal.com>
2023-07-10 20:08:51 +0530

    Data Returning succesfully

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express()
 mongoose.connect(url, {useNewUrlParser:true})
 const con = mongoose.connection
 
-con.on('open', function(){
+con.on('open', () => {
     console.log('connected...')
+})
+
+const alienRouter = require('./routes/aliens')
+app.use('/aliens' ,alienRouter)
+
+app.listen(9000, () => {
+    console.log('Server Started')
 })
---
+++ routes/aliens.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const Alien = require('../models/alien')
+
+router.get('/', async(req, res) => {
+    try{
+        const aliens = await Alien.find()
+        res.json(aliens)
+    }
+    catch(err){
+        res.send('Error' + err)
+    }
+})
+
+module.exports = router
